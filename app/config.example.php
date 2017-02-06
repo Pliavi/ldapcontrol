f036f8fbef81a2da0ad7a1454fe88c9ba94235af
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-06 17:48:45 -0200

    Adição de arquivo de configuração exemplo

---
+++ app/config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+// const ADCONFIG = [
+//     // Server
+//     'domain_controllers' => ['127.0.0.1', 'localhost'],
+//     'base_dn'            => 'dc=example,dc=com',
+//     'ad_port'            => 389,
+//     'use_ssl'            => 0,
+
+//     // Admin
+//     'admin_username' => 'PREFIX\USERNAME',
+//     'admin_password' => '************',
+//     'admin_prefix' => 'PREFIX\\', // Essa propriedade não existe no adldap, mas criei pra facilitar o login
+
+//     // Contas
+//     'account_suffix' => '@example.com'
+// ];
