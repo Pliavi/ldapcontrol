544b8d2337098d5f92e24bcfa74d1ea026cc24e2
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-06 17:50:36 -0200

    busca all() filtrada por apenas OU=Grupos

--- app/controllers/GroupController.php
+++ app/controllers/GroupController.php
@@ -1,9 +1,20 @@
 <?php
 
+use \Adldap\Schemas\ActiveDirectory as ActiveDirectory;
+
 class GroupController extends LdapController {
 
     public function all() {
-        return $this->ad->groups()->all()->toArray();
+        $groups = $this->ad->groups()->all()->toArray();
+        $fGroups = [];
+        foreach ($groups as $group) {
+            if(explode(',', $group['dn'])[1] == "OU=Grupos"){
+                if(count(explode('-', $group['name'][0])) == 1 && $group['name'][0] !== "DESKTOP PERSONALIZATION"){
+                    $fGroups[] = $group;
+                }
+            }
+        }
+        return $fGroups;
     }
 
 }
