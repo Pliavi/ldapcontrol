c5a033cbced4de65936c522de7cc7e606e6281b8
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-06 17:52:39 -0200

    update 06-02 17-52

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,60 +1,76 @@
 <?php
+use \Adldap\Objects\AccountControl as AccountControl;
 
 Class UserController extends LdapController {
     
     private $validation = [
-        'userPrincipalName'  => 'required', // Matrícula
-        'cn'                 => 'required', // Nome do usuário
-        // 'sectorFolderAccess' => 'required', // Pasta "g:/"
-        // 'groups'             => 'required',
+        'userPrincipalName'  => 'required|numeric', # Matrícula
+        'cn'                 => 'required', # Nome do usuário
+        'group'              => 'required',
     ];
 
     public function create(){
+        # VARS
         $data = Input::all();
-        $data['sAMAccountName'] = $data['userPrincipalName']; // Seta o Accountname de acordo com o PrincipalName
-        $data['scriptPath'] = 'seduc.vbs';//ADSCRIPT[$data['groups']]; // Setar o script que mapeia as unidades
+        $cn = mb_strtoupper($data['cn'], 'UTF-8'); # Nome em letra maiúscula
+        $userPrincipalName = $data['userPrincipalName']; # MAtrícula
+        $sAMAccountName = $data['userPrincipalName']; # Seta o Accountname(Win2000) de acordo com o $userPrincipalName
+        $groupName = $data['group'];
+        $groupDn = "";
+        $ou = "OU=Usuarios";
+        $dn = "";
+        $scriptPath = ADSCRIPT[$groupName] or 'empty.vbs';
 
         try {
-            $user = $this->ad->users()->newInstance();
-            $user->cn = "CobaiaA1";
-            $user->samAccountName = "coba1";
-            $user->userPrincipalName = "coba1";
-            $user->dn = 'cn=CobaiaA1,dc=pmc,dc=local';
+            # Seleciona o grupo ao qual o usuário será adicionado
+            $group = $this->ad->groups()->search()->whereEquals('name', $groupName)->firstOrFail();
 
-            if($user->save()){
-                echo "Ok";
-            }else{
-                echo "ERROR<br>";
-                die($this->ad->getConnection()->getLastError());                
+            # Seleciona as Unidades Organizacionais a serem adicionadas ao DN (não testado)
+            # Caso não funcione, utilizar o modelo de OrganizationalUnit do Adldap2;
+            $groupDn = $group['dn'];
+            $groupDn = (explode(",", $groupDn));
+
+            foreach($groupDn as $partial) { 
+                # do DN do grupo, pega apenas as OU e as que não seja de Grupo (OU=Grupos)
+                if(substr($partial, 0, 2) == "OU" && !strpos($partial, 'Grupos')){
+                    $ou .= ",$partial";
+                }
             }
 
+            # Cria o usuário
+            $dn = "cn=$cn,$ou,dc=pmc,dc=local";
 
+            $user = $this->ad->users()->newInstance();
+            $user->cn = $cn;
+            $user->userPrincipalName = $userPrincipalName;
+            $user->sAMAccountName = $sAMAccountName;
+            $user->dn = $dn;
+            $user->scriptPath = $scriptPath;
+            
+            # Ativa o usuário (não testado, caso não funcione, testar executar após criar o usuário)
+            $accountControl = new AccountControl;
+            $accountControl->accountIsNormal();
+            $user->setUserAccountControl($accountControl);
 
-            // $success = $this->ad->users()->create($data);
-            // if(!$success){ 
-            //     die($this->ad->getConnection()->getLastError());
-            //     // throw new AdldapException("Falha ao criar o usuário"); 
-            // }
+            if($user->save()) {
+                Session::flash('success', "Novo usuário: $userPrincipalName cadastrado.");
+                Log::info("Novo usuário: $userPrincipalName cadastrado.");
+            } else {
+                throw new Exception($this->ad->getConnection()->getLastError());
+            }
 
-            // $user = $this->ad->search()->users()->find($data['userPrincipalName']);
+            # Adiciona o usuário ao grupo selecionado acima
+            $group->addMember($user);
 
-            // foreach ($data['groups'] as $group) {
-            //     $groupToAdd = $ad->search()->groups()->firstOrFail($group);
-            //     $groupToAdd->addMember($user);
-            // }
-
-            // Log::info("Novo usuário: {$data['userPrincipalName']} cadastrado.");
-            // Session::flash('success', "Novo usuário: {$data['userPrincipalName']} cadastrado.");
-
-            // return Redirect::back();
-        } catch(AdldapException $e) { 
-            Log::error("Algo impossibilitou a criação do usuário: ".$e->getMessage());
-            Session::flash("Algo impossibilitou a criação do usuário: ".$e->getMessage());
-        } catch(ModelNotFoundException $e){
+            return Redirect::back();
+        } catch(ModelNotFoundException $e) {
             Log::error("Não foi possível encontrar o grupo solicitado durante o cadastro do novo usuário: ".$e->getMessage());
-            Session::flash("Não foi possível encontrar o grupo solicitado durante o cadastro do novo usuário: ".$e->getMessage());
-        }catch(Exception $e){
-            echo $e->getMessage();
+            Session::flash('error', "Não foi possível encontrar o grupo solicitado durante o cadastro do novo usuário: ".$e->getMessage());
+            return Redirect::back();            
+        } catch(Exception $e) {
+            Log::error("Algo impossibilitou a criação do usuário: ".$e->getMessage());
+            Session::flash('error', "Algo impossibilitou a criação do usuário: ".$e->getMessage());
+            return Redirect::back();            
         }
     }
 }
