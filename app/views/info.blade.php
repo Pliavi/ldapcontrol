3a37b6b11cd49ac4aefd5b12a80ea29717e3187e
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-10 17:33:52 -0200

    added confirmation help alerts to the page

--- app/views/info.blade.php
+++ app/views/info.blade.php
@@ -1,7 +1,23 @@
 @include('includes.head')
-<header>Dados do Usuário</header>
+<?php $confirmation = isset($confirmation); ?>
+
+@if($confirmation)
+    <header>Confirmação dos Dados</header>
+@else
+    <header>Dados do Usuário</header>
+@endif
+
 <div class="grid">
     @include('includes.notification')
+
+    @if($confirmation)
+    <div class="alert">
+        <b>Confirme os dados de {{ $data['name'] }} (Matrícula: {{ $data['id'] }}) abaixo:</b><br>
+        Caso estejam corretos, clique em <b><i>Concluir</i></b> para concluir o cadastro. <br>
+        Caso algo esteja errado, clique em <b><i>Voltar</i></b> para voltar ao formulário.
+    </div>
+    @endif
+
     <label>Matrícula:</label> {{ $data['id'] }}  <br>
 
     <label>Nome:</label> {{ $data['name'] }} <br>
@@ -23,6 +39,12 @@
         <li>Usuário não pertence a nenhum grupo</li>
     @endforelse
     </ul>
+
+    @if($confirmation)
+    <a href="{{ route('createUser') }}" class="btn">Concluir</a>
+    <a href="{{ route('newUserView') }}" class='btn-outline'>Voltar</a>
+    @else
     <a href="{{ route('findUserView') }}" class='btn-outline'>Voltar</a>
+    @endif
 </div>
 @include('includes.footer')
