38ac104cbad893305bccffae6513f8abadb5575d
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-10 17:37:51 -0200

    removed unused boostrap and added logout button

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,8 +1,7 @@
 <html>
 <head>
-    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> --}}
-    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"> --}}
     <link rel="stylesheet" href="{{ asset('assets/css/siimple.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 </head>
 <body>
+<div class='pl-logout'>{{Session::get('auth')}} <a class="btn btn-outline cover-btn" href="{{ route('logout') }}">Sair</a></div>
