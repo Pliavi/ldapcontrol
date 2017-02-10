29cc8d57bdee7c35630c80695af962f7c5186f61
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-10 17:29:20 -0200

    added filters to routes, login & validator extends

--- app/routes.php
+++ app/routes.php
@@ -1,20 +1,29 @@
 <?php
+### Extend Validator
+Validator::extend('alpha_spaces', function($attribute, $value) {
+    return preg_match('/^[\pL\s]+$/u', $value);
+});
+
 ### LOGIN
+Session::keep(['auth']); # Mantém o usuário logado
 Route::get('login', 'LoginController@loginView');
 Route::post('signin', 'LoginController@login');
+Route::get('logout', [ 'as' => 'logout', 'uses' => 'LoginController@logout']);
 
-### USUÁRIO
-### \ Views
-Route::get('/',                  [ 'as' => 'findUserView'        , 'uses' => 'UserController@findView' ]);
-Route::get('/novo_usuario',      [ 'as' => 'newUserView'         , 'uses' => 'UserController@newUserView' ]);
-Route::get('/info_usuario',      [ 'as' => 'userInfoView'        , 'uses' => 'UserController@infoView' ]);
-Route::post('/confirma_usuario', [ 'as' => 'userConfirmationView', 'uses' => 'UserController@confirmationView' ]);
-### \ Funções
-Route::get('/usuario', [ 'as' => 'findUser',   'uses' => 'UserController@find' ]);
-Route::post('/criar_usuario',    [ 'as' => 'createUser', 'uses' => 'UserController@create' ]);
+Route::group(['before' => 'auth'], function(){
+    ### USUÁRIO
+    ### \ Views
+    Route::get('/',                  [ 'as' => 'findUserView'        , 'uses' => 'UserController@findView' ]);
+    Route::get('/novo_usuario',      [ 'as' => 'newUserView'         , 'uses' => 'UserController@newUserView' ]);
+    Route::get('/info_usuario',      [ 'as' => 'userInfoView'        , 'uses' => 'UserController@infoView' ]);
+    Route::post('/confirma_usuario', [ 'as' => 'userConfirmationView', 'uses' => 'UserController@confirmationView' ]);
+    ### \ Funções
+    Route::post('/usuario', [ 'as' => 'findUser',   'uses' => 'UserController@find' ]);
+    Route::get('/criar_usuario',    [ 'as' => 'createUser', 'uses' => 'UserController@create' ]);
 
-### "API"
-Route::post('/groups', 'GroupController@all');
+    ### "API"
+    Route::post('/groups', 'GroupController@all');
+});
 
 ### TESTES
 Route::get('test', 'TestController@test');
