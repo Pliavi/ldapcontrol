cee9e05673629638a6d79fb267f47cb968108289
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-10 17:38:13 -0200

    added array error possibility

--- app/views/includes/notification.blade.php
+++ app/views/includes/notification.blade.php
@@ -1,6 +1,20 @@
 @if(Session::has('error'))
-    <div class="alert alert-error">{{ Session::get('error') }}</div>
+    <?php $error = Session::pull('error'); ?>
+    <div class="alert alert-error">
+    @if(is_array($error))
+    <b>Alguns problemas foram encontrados:</b>
+        <ul>
+        @foreach($error as $field => $err)
+            @foreach($err as $e)
+                <li>{{ $e }}</li>
+            @endforeach
+        @endforeach
+        </ul>
+    @else
+        {{ $error }}
+    @endif
+    </div>
 @endif
 @if(Session::has('success'))
-    <div class="alert alert-done">{{ Session::get('success') }}</div>                    
+    <div class="alert alert-done">{{ Session::pull('success') }}</div>                    
 @endif
