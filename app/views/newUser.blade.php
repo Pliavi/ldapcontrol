a64dd0623b41a2eb05aa4fa8d7a9d78923a92832
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-10 17:34:31 -0200

    some attr improvements and old value getters

--- app/views/newUser.blade.php
+++ app/views/newUser.blade.php
@@ -1,24 +1,38 @@
 @include('includes.head')
-<header>Novo Usuário (Mt. {{$id}})</header>
+<header>Novo Usuário (Mt. {{ Session::get('id') }})</header>
 <form action="{{ route('userConfirmationView') }}" method="POST" class='grid'>
     @include('includes.notification')
 
     <label>Nome</label>
-    <input type='text' name='cn' autofocus required class='form-input'>
+    <input type='text' name='cn' 
+        pattern = '(\b[a-zA-Z\u00C0-\u017F]+\b|\s){2,}'
+        placeholder = 'Nome e Sobrenome'
+        autofocus 
+        required 
+        class='form-input' 
+        value="{{ Input::old('cn') }}">
 
     <label>Descrição</label>
-    <input type='text' name='description' required class='form-input'>
+    <input 
+        type='text' 
+        name='description' 
+        class='form-input' 
+        value="{{ Input::old('description') }}">
 
     <label>Grupo</label>
     <select name='group' required class='form-select'>
         <option value=''>-- Selecione um grupo --</option>
         @foreach($groups as $group)
+        @if(Input::old('group') == $group)
+            <option value="{{ $group }}" selected>{{ $group }}</option>        
+        @else
             <option value="{{ $group }}">{{ $group }}</option>
+        @endif
         @endforeach
     </select>
 
-    <a href="{{ route('findUserView') }}" class='btn-outline'>Cancelar</a>
     <button type='submit' class='btn'>Enviar</button>
+    <a href="{{ route('findUserView') }}" class='btn-outline'>Cancelar</a>
 
 </form>
 @include('includes.footer')
