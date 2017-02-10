d13a7970a689c559969a7a248883ad67839c4851
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-02-10 17:29:55 -0200

    it's just tests :v

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -6,7 +6,16 @@ class TestController extends LdapController {
 
     public function test() {
         // return json_encode($this->ad->users()->search()->whereEquals('sAMAccountname', '20080')->firstOrFail());//->name[0];
-        return json_encode($this->ad->users()->search()->whereEquals('name', 'teste2')->firstOrFail());//->name[0];
+        $user = $this->ad->users()->search()->whereEquals('samaccountname', '20080')->firstOrFail();
+        // $user->lockouttime = 0;
+        // $user->pwdlastset = 0;
+        // $user->useraccountcontrol = 544;
+        return json_encode($user);
+        // if($user->save()){
+        //     echo "deu";
+        // }else{
+        //     echo 'errpo'. $this->ad->getConnection()->getLastError();
+        // }
     }
 
 }
